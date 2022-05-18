2607d7930830aa1a071533821feec8366cf877ad
Monique <veldmanmonique@gmail.com>
2022-05-18 14:26:07 +0000

    Add myself

--- sql-crud.py
+++ sql-crud.py
@@ -80,16 +80,25 @@ tim_berners_lee = Programmer(
     famous_for="World Wide Web"
 )
 
+monique_veldman = Programmer(
+    first_name="Monique",
+    last_name="Veldman",
+    gender="F",
+    nationality="Dutch",
+    famous_for="Code Institute Student"
+)
+
 # add each instance of our programmers to our session
-# session.add(ada_lovelace)
-# session.add(alan_turing)
-# session.add(grace_hopper)
-# session.add(margaret_hamilton)
-# session.add(bill_gates)
-# session.add(tim_berners_lee)
+session.add(ada_lovelace)
+session.add(alan_turing)
+session.add(grace_hopper)
+session.add(margaret_hamilton)
+session.add(bill_gates)
+session.add(tim_berners_lee)
+session.add(monique_veldman)
 
 # commit our session to the database
-# session.commit()
+session.commit()
 
 
 # query the database to find all Programmers
